44ade689cc349fb5325f95f87fd1a6ae0fcc3314
mohprilaksono <mohprilaksono@gmail.com>
2021-06-05 15:53:05 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mohprilaksono/app-hello
+
+go 1.16
+
+require github.com/mohprilaksono/go-hello/v2 v2.0.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_hello "github.com/mohprilaksono/go-hello/v2"
+)
+
+func main() {
+	fmt.Println(go_hello.Hello("Akson"))
+}
